b6b8d4b5bb5ac0c2f2fcc32024603c04eb9f744c
Chip <chip.elmer@gmail.com>
2018-08-20 14:01:12 -0500

    Added UI class and refactored to use

---
+++ OpenHouseHW/UI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenHouseHW
+{
+    public static class UI
+    {
+        public static bool UserAnswersYes(string question)
+        {
+            // TODO: improve this method and make it cleaner
+
+            Console.WriteLine(question);
+
+            string response = Console.ReadLine().ToUpper();
+
+            if(response == "YES" || response == "Y")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
